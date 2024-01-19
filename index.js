0bb7776ba50f3bc2fcba31a6a956cd7efaae6037
DanielHaro28 <149003502+DanielHaro28@users.noreply.github.com>
2024-01-19 02:03:13 +0000

    ahhhhhhhh

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const wss = new WebSocket.Server({ server:server });
 
+ var Clients = [];
+
 const port = process.env.PORT || 1235;
 
 app.get('/', function(req, res) {
@@ -35,13 +37,20 @@ console.log('Server started at http://localhost:' + port);
 
 
 wss.on('connection', function connection(wss) {
-  wss.on('message', function incoming(message) {
-    console.log('received: %s', message);
 
-    wss.send('something');
-    wss.id = message;
-    wss.id.send()
-  });
+     id = Math.random();
+     Clients[id] = wss;
+
+     wss.on('message', function incoming(message) {
+      if (message != "start") {
+        for (var i in Clients) {
+          Clients[i].send(message);
+        }
+      }
+
+    });
 });
+
+
 
 server.listen(8080,() => console.log('Socket escuchando puerto 8080'));
