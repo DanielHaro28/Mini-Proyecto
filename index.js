ac170083616c2681d4bb652fe133085c5ddc78ed
DanielHaro28 <149003502+DanielHaro28@users.noreply.github.com>
2024-01-21 00:24:29 +0000

    cambio en el index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require('console')
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -6,7 +7,11 @@ const server = app.listen(port, () => console.log(`Listening on port ${port}`))
 
 var anfitrion = '';
 let partidaEmpezada = false;
-let contadorjugadores = '';
+let playercont = '';
+let playerList = [];
+let playerstatus = [];
+let usuario = '';
+let connecstatus = false;
 
 const io = require('socket.io')(server, {
   cors: {
@@ -16,13 +21,11 @@ const io = require('socket.io')(server, {
 
 
 io.on('connection', (socket) => {
-  contadorjugadores = io.engine.clientsCount;
-  console.log(contadorjugadores);
+
   socket.on('enviar-palabra-todos', (palabra, id) => {
     anfitrion = id;
     partidaEmpezada = true;
-    
-    socket.broadcast.emit('palabra-recibir', palabra, id)
+    socket.broadcast.emit('palabra-recibir', palabra, id)   
   });
 
   socket.on('letra-enviada', (letra, idjugador, usuario) => {
@@ -33,39 +36,64 @@ io.on('connection', (socket) => {
     socket.to(idjuga).emit('respuesta-a-usuario', respuesta)
   });
 
-  socket.on('ganador', (usuario) =>{
+  socket.on('ganador', (usuario) => {
     socket.broadcast.emit('ganador-a-todos', usuario)
   });
 
-  socket.on('perdedor', (usuario) =>{
+  socket.on('perdedor', (usuario) => {
     socket.to(anfitrion).emit('perdedor-a-anfitrion', usuario)
   });
-  
-  socket.on('partida-empezada',(callback)=>{
-    callback( partidaEmpezada )
+
+  socket.on('partida-empezada', (callback) => {
+    callback(partidaEmpezada)
   })
 
+  socket.on('registro-usuario', (id) => {
+    playerList.push(id);
+    playerstatus.push(true);
+    playercont = playerList.length;
+  });
+
+  socket.on('connect_failed', () => {
+    console.log('error');
+  });
+
+  socket.on('reconnect_failed', () => {
+    console.log('error');
+  });
+
+  socket.on('disconnect', () => {
+    for (let i = 0; i < playerList.length; i++) {
+      socket.to(playerList[i]).emit('verify', (callback) => {
+        connecstatus = callback;
+        if (connecstatus == null) {
+          playerstatus[i] = false;
+        }
+      })
+    }
+  })
 });
+
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname + '/public/images')));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/homepage.html'));
 });
 
-app.get('/admin', function(req, res) {
+app.get('/admin', function (req, res) {
   res.sendFile(path.join(__dirname, '/admin.html'));
 });
 
-app.get('/index', function(req, res) {
+app.get('/index', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/playername', function(req, res) {
+app.get('/playername', function (req, res) {
   res.sendFile(path.join(__dirname, '/playername.html'));
 });
 
-app.get('/admingame', function(req, res) {
+app.get('/admingame', function (req, res) {
   res.sendFile(path.join(__dirname, '/admingame.html'));
 });
